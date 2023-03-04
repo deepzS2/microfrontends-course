0e806f8c06677b9c2c28a48b234df7a3f51bf93e
deepzS2 <alanbackman10@hotmail.com>
2023-03-04 17:11:03 -0300

    feat: adding production prefixes for class names

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,10 @@
+import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
+
+ClassNameGenerator.configure((componentName) => `co-${componentName}`)
 
 export default function App() {
   return (
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,9 +1,11 @@
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ThemeProvider, createTheme, unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material'
 import React from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
+
+ClassNameGenerator.configure((componentName) => `ma-${componentName}`)
 
 export default function App() {
   return (
